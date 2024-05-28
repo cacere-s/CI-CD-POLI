959729f39d168d070c7004ed5422af281819f077
fflorez8409 <convers8409@gmail.com>
2024-05-27 21:15:12 -0500

    Fabian Florez: Configuracion de cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import express from 'express';
+import cors from 'cors'
 
 const app = express();
-
-
 app.use(express.json());
+
+const corsOption = {
+  origin: '*', 
+  optionsSuccessStatus:200,
+  credentials:true
+}
+app.use(cors(corsOption))
+
+// route imports 
 import userRouter from './routes/userRouter.js'
+
+// middlewar imports
 app.use('/api/v1/auth', userRouter)
 
 const PORT = process.env.PORT || 8080
